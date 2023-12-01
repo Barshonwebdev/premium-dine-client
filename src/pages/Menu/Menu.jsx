04eabbdc520d48ebde61f65f3fc691151ae3d767
Barshon <barshonweb@gmail.com>
2023-12-01 13:08:38 +0600

    parallax cover

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Helmet } from "react-helmet-async";
+import Cover from '../Shared/Cover';
+import coverImage from '../../assets/menu/banner3.jpg'
 const Menu = () => {
     return (
         <div>
             <Helmet>
                 <title>Premium Dine | Menu</title>
             </Helmet>
+
+            <Cover heading={'Our Menu'} subHeading={'Would you like to try a dish ?'} img={coverImage}></Cover>
         </div>
     );
 };
---
+++ src/pages/Shared/Cover.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Parallax, Background } from "react-parallax";
+const Cover = ({img, heading, subHeading}) => {
+    return (
+      <div
+        className="hero h-[600px] bg-fixed"
+        style={{
+          backgroundImage:
+            `url(${img})`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content bg-black bg-opacity-50 rounded-lg md:px-60 md:py-14 px-8 py-5">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold uppercase">{heading}</h1>
+            <p className="mb-5">
+             {subHeading}
+            </p>
+            
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Cover;
