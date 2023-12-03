1e820e3cb9c8bdbda7f68ede24dd37cc0721888e
Barshon <barshonweb@gmail.com>
2023-12-03 23:04:09 +0600

    data loaded from server

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -5,16 +5,20 @@ const FoodCard = ({item}) => {
 
     return (
       <div className="card card-compact w-fit bg-base-100 shadow-xl">
-        <p className="bg-gray-900 text-orange-300 absolute right-0 mt-5 mr-5 p-1 rounded-lg">$ {price}</p>
+        <p className="bg-gray-900 text-orange-300 absolute right-0 mt-5 mr-5 p-1 rounded-lg">
+          $ {price}
+        </p>
         <figure>
           <img src={image} alt="Shoes" />
         </figure>
-        <div className="card-body">
+        <div className="card-body flex flex-col items-center">
           <h2 className="card-title">{name}</h2>
 
           <p>{recipe}</p>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Order Now</button>
+            <button className="btn btn-outline hover:bg-black hover:text-amber-500 border-black text-xl italic border-0 border-b-4 my-5">
+              Order 
+            </button>
           </div>
         </div>
       </div>
