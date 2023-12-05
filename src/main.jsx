6d8096d6d2564b17509367cf302e11e0e7b36910
Barshon <barshonweb@gmail.com>
2023-12-05 17:00:26 +0600

    firebase and context setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import {  RouterProvider } from "react-router-dom";
 import router from './routes/Routes.jsx';
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from './context/AuthProvider.jsx';
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <div className="max-w-screen-lg mx-auto">
-        <RouterProvider router={router} />
-      </div>
-    </HelmetProvider>
+    <AuthProvider>
+      <HelmetProvider>
+        <div className="max-w-screen-lg mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </HelmetProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -32,14 +32,10 @@ const Login = () => {
     }
     return (
       <div className="hero min-h-screen ">
-        <div className="hero-content flex-col lg:flex-row">
+        <div className="hero-content flex-col md:space-x-28 lg:flex-row">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+            <h1 className="text-5xl font-bold">Login!</h1>
+            
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <form onSubmit={handleLogin} className="card-body">
