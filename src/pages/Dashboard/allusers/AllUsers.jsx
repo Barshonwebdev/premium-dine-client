98ce38d105ab966baedb5296af5d58c414f7284d
Barshon <barshonweb@gmail.com>
2023-12-10 10:38:45 +0600

    make admin implemented

--- src/pages/Dashboard/allusers/AllUsers.jsx
+++ src/pages/Dashboard/allusers/AllUsers.jsx
@@ -2,15 +2,31 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { FaUsers } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
 const AllUsers = () => {
-    const {data: users=[], }=useQuery({queryKey:['users'] ,queryFn: async()=>{
+    const {data: users=[], refetch}=useQuery({queryKey:['users'] ,queryFn: async()=>{
         const res= await fetch('http://localhost:5000/users')
         return res.json();
     }})
 
-    const handleMakeAdmin=(id)=>{
-
+    const handleMakeAdmin=(user)=>{
+        fetch(`http://localhost:5000/users/admin/${user._id}`,{
+          method:"PATCH",
+        })
+        .then(res=>res.json())
+        .then(data=>{
+          if(data.modifiedCount>0){
+            refetch();
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: `${user.name} has been made admin `,
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
+        })
     }
     const handleDelete=(user)=>{
 
@@ -44,7 +60,7 @@ const AllUsers = () => {
                       {user.role === "admin" ? (
                         "Admin"
                       ) : (
-                        <button onClick={()=>handleMakeAdmin(user._id)} className='btn bg-orange-500'>
+                        <button onClick={()=>handleMakeAdmin(user)} className='btn bg-orange-500'>
                           <FaUsers className="text-lg"></FaUsers>
                         </button>
                       )}
