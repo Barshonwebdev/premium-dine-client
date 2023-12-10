a367e64cf74f9f89e522b1b76a918f60c4472e28
Barshon <barshonweb@gmail.com>
2023-12-10 13:56:03 +0600

    jwt token saved in local storage

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -12,6 +12,7 @@ import {
 
 
 import { app } from '../firebase/firebase.config';
+import axios from 'axios';
 export const AuthContext=createContext(null);
 const auth=getAuth(app);
 const provider = new GoogleAuthProvider();
@@ -23,6 +24,17 @@ const AuthProvider = ({children}) => {
         const unsubscribe=onAuthStateChanged(auth,currentUser=>{
             setUser(currentUser);
             console.log('current user :',currentUser);
+
+            if(currentUser){
+                axios.post("http://localhost:5000/jwt",{Email: currentUser.email})
+                .then(data=>{
+                    console.log(data.data.token)
+                    localStorage.setItem('access-token',data.data.token);
+                })
+            }
+            else{
+                localStorage.removeItem('access-token');
+            }
             setLoading(false)
         });
         return ()=>{
