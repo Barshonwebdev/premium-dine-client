f90e467da4739550918ddba15387eb19f9bd3dec
Barshon <barshonweb@gmail.com>
2023-12-12 17:03:53 +0600

    image upload to imgbb

--- src/pages/Dashboard/additem/AddItem.jsx
+++ src/pages/Dashboard/additem/AddItem.jsx
@@ -1,16 +1,32 @@
 import { useForm } from 'react-hook-form';
 import SectionTitle from '../../../components/Sectiontitle/SectionTitle';
 
+const img_hosting_token = import.meta.env.VITE_IMAGE_HOSTING_TOKEN;
+
 const AddItem = () => {
+
      const {
        register,
        handleSubmit,
        formState: { errors },
      } = useForm();
      const onSubmit = (data) => {
-        console.log(data)
+        console.log(data);
+        const formData= new FormData();
+        formData.append('image', data.image[0]);
+
+        fetch(img_hosting_url, {
+            method:'POST',
+            body:formData,
+        })
+        .then(res=>res.json())
+        .then(imgResponse=>{
+            console.log(imgResponse);
+        })
+        
     };
-     console.log(errors);
+
+     const img_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${img_hosting_token}`;
     return (
       <div className="w-full px-8">
         <SectionTitle
