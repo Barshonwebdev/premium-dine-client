3fad3c9252a7b05f22fd98ac76f09c05c663aecf
Barshon <barshonweb@gmail.com>
2023-12-12 13:53:04 +0600

    hook form for add item form

--- src/components/Sectiontitle/SectionTitle.jsx
+++ src/components/Sectiontitle/SectionTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SectionTitle = ({heading,subHeading}) => {
     return (
-        <div className='md:w-3/12 w-2/3 mx-auto text-center my-8'>
+        <div className='md:w-5/12 w-2/3 mx-auto text-center my-8'>
             <p className='text-yellow-600 italic mb-2'>--{subHeading}--</p>
             <h3 className='text-xl md:text-3xl uppercase border-y-4 border-gray-800 py-3'>{heading}</h3>
         </div>
--- src/pages/Dashboard/additem/AddItem.jsx
+++ src/pages/Dashboard/additem/AddItem.jsx
@@ -1,10 +1,78 @@
-import React from 'react';
+import { useForm } from 'react-hook-form';
+import SectionTitle from '../../../components/Sectiontitle/SectionTitle';
 
 const AddItem = () => {
+     const {
+       register,
+       handleSubmit,
+       formState: { errors },
+     } = useForm();
+     const onSubmit = (data) => {
+        console.log(data)
+    };
+     console.log(errors);
     return (
-        <div>
-            <h3 className='text-3xl'>Add an Item</h3>
-        </div>
+      <div className="w-full px-8">
+        <SectionTitle
+          subHeading={"What`s new?"}
+          heading={"Add an Item"}
+        ></SectionTitle>
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div className="form-control w-full max-w-xs">
+            <label className="label">Recipe Name*</label>
+            <input
+              {...register("name", { required: true, maxLength: 80 })}
+              type="text"
+              placeholder="recipe name"
+              className="input input-bordered w-full max-w-xs"
+            />
+          </div>
+          <div className="flex justify-between flex-col md:flex-row">
+            <div>
+              <label className="label">Category*</label>
+              <select
+                {...register("category", { required: true })}
+                className="select select-bordered w-full max-w-xs"
+              >
+                <option disabled selected>
+                  category
+                </option>
+                <option>Salad</option>
+                <option>Pizza</option>
+                <option>Soup</option>
+                <option>Dessert</option>
+                <option>Drinks</option>
+              </select>
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">Price*</label>
+              <input
+                {...register("price", { required: true })}
+                type="number"
+                placeholder="$"
+                className="input input-bordered w-full max-w-xs"
+              />
+            </div>
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className="label">Recipe Detail*</label>
+            <textarea
+              {...register("recipe", { required: true })}
+              className="textarea textarea-bordered h-32"
+              placeholder="recipe in detail"
+            ></textarea>
+          </div>
+          <div className="form-control w-full max-w-xs">
+            <label className='label'>Image*</label>
+            <input
+              {...register("image", { required: true})}
+              type="file"
+              className="file-input w-full max-w-xs"
+            />
+          </div>
+          <input className="btn btn-primary" type="submit" value="Add Item" />
+        </form>
+      </div>
     );
 };
 
