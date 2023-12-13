6f62a1c368729cda2217228be79a8896ce73c8ad
Barshon <barshonweb@gmail.com>
2023-12-13 17:21:11 +0600

    test card

--- src/pages/Dashboard/payment/CheckoutForm.jsx
+++ src/pages/Dashboard/payment/CheckoutForm.jsx
@@ -1,43 +1,69 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import React from 'react';
+import { useState } from 'react';
 
 const CheckoutForm = () => {
+    const [cardError, setCardError] = useState("");
     const stripe=useStripe();
     const elements=useElements();
-    const handleSubmit=(event)=>{
+    const handleSubmit=async(event)=>{
         event.preventDefault();
+         if (!stripe || !elements) {
+           return;
+         }
+          const card = elements.getElement(CardElement);
+        
+         if(card===null){
+            return 
+         }
+
+         const {error,paymentMethod}= await stripe.createPaymentMethod({
+            type:'card',
+            card,
+         })
+        
+        if(error){
+            console.log(error.message);
+            setCardError(error.message);
+        }
+        else {
+            setCardError('');
+            console.log(paymentMethod);
+        }
+         
     }
 
-    if(!stripe || !elements){
-        return
-    }
+   
 
-    const card=elements.getElement(CardElement);
-    if(card===null){
-        return
-    }
+   
     return (
-      <form onSubmit={handleSubmit}>
-        <CardElement
-          options={{
-            style: {
-              base: {
-                fontSize: "16px",
-                color: "#424770",
-                "::placeholder": {
-                  color: "#aab7c4",
+      <>
+        <form onSubmit={handleSubmit}>
+          <CardElement
+            options={{
+              style: {
+                base: {
+                  fontSize: "16px",
+                  color: "#424770",
+                  "::placeholder": {
+                    color: "#aab7c4",
+                  },
+                },
+                invalid: {
+                  color: "#9e2146",
                 },
               },
-              invalid: {
-                color: "#9e2146",
-              },
-            },
-          }}
-        />
-        <button type="submit" disabled={!stripe}>
-          Pay
-        </button>
-      </form>
+            }}
+          />
+          <button
+            className="btn btn-primary btn-sm mt-5 hover:bg-blue-950"
+            type="submit"
+            disabled={!stripe}
+          >
+            Pay
+          </button>
+        </form>
+        <div>{cardError && <p className='text-red-700 mt-2'>{cardError}</p>}</div>
+      </>
     );
 };
 
--- src/pages/Dashboard/payment/Payment.jsx
+++ src/pages/Dashboard/payment/Payment.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+
 import SectionTitle from '../../../components/Sectiontitle/SectionTitle';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
 
 const Payment = () => {
+    
+    const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PK);
     return (
         <div className='w-full px-10'>
             <SectionTitle subHeading={'please pay by your card'} heading={'Payment'}></SectionTitle>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm></CheckoutForm>
+            </Elements>
         </div>
     );
 };
