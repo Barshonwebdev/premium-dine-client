72ff215a7ee2defa4f50bb2e0a66e2e212bc3ca3
Barshon <barshonweb@gmail.com>
2023-12-14 11:30:45 +0600

    private route redirect fix

--- src/routes/PrivateRoutes.jsx
+++ src/routes/PrivateRoutes.jsx
@@ -7,14 +7,16 @@ const PrivateRoutes = ({children}) => {
     const location=useLocation();
     
 
-        if(loading){
-            <progress className="progress w-56"></progress>;
-        }
-     if (user) {
-       return children;
-     }
+            if(loading)
+            {
+               return <progress className="progress w-56"></progress>;
+            }
+            if (user) 
+            {
+            return children;
+            }
        
-        else return <Navigate to='/login' state={{from:location}} replace></Navigate>
+             else return <Navigate to='/login' state={{from:location}} replace></Navigate>
         
     ;
 };
